c91f21e48d6b8c7ac1b9d1a3e2776aa99153e42d
D1D7 <152475650+D1D7@users.noreply.github.com>
2024-03-05 00:17:30 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-// Write your code here
+
+2 == "2;
+2 === 2;
+10 % 3 === 0;
+10 % 3 === 1;
+true && false;
+false || true;
+true || false;
+5 + 8 >= 12 || 19 < 90 || !false;
+"Josh" == "josh" && !true && false && 17.9 + 0.1 == 18.1;
+0.1 + 0.2 == 0.3;
+Math.min(0, 1, 2, 10, 11, 100) && Math.max(-100, -90, -1, -0.9, 0);
+Math.max(0, 1, 2, 10, 11, 100) && Math.min(-100, -90, -1, -0.9, 0);
+Math.min(0, 1, 2, 10, 11, 100) || Math.min(-100, -90, -1, -0.9, 0);
+!Math.min(0, 1, 2, 10, 11, 100) || !Math.max(-100, -90, -1, -0.9, 0);
+
+
+
+2 == "2"; - true
+2 === 2; - true
+10 % 3 === 0; - false
+10 % 3 === 1; - true
+true && false; - false
+false || true; - true
+true || false; - true
+5 + 8 >= 12 || 19 < 90 || !false; - true
+"Josh" == "josh" && !true && false && 17.9 + 0.1 == 18.1; - false
+0.1 + 0.2 == 0.3; - false
+
+0
+-0.9
+-100
+
